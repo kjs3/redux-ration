374eb6542de35167684f6555335e3ba333774fdc
Kenny Smith <kjs3@kjs3.com>
2015-09-22 21:21:02 -0400

    making api request and showing results

--- README.md
+++ README.md
@@ -1,28 +1,11 @@
 # Redux Ration
 
-### Step 2
+### Step 3
 
-##### Moving some files around
+##### Getting food recommendations from Foursquare
 
-* moved application files into `src/` directory
-* `index.html` is now in `src/` directory
-* `index.scss` is now in `src/styles` directory
-* moved `App` React class out of `index.js` and into a `src/components/app.jsx` module
-* changed from es6 `class` syntax to `React.createClass` which allows mixins and binds `this` to methods
+* when form is submitted the query is used to search Foursquare for food recommendations
+* using pure DOM API as we're not conserned with older browsers
+* everything is still handled in a single component
 
-##### Setting up the basic structure of the page
-* the root HTML node of the app is a `main` tag
-* it includes a header, `search-form` section and a `search-results` section
-* added `normalize.css` module as `package.json` dependency and imported from within `index.scss`
-* added some basic styling
-
-##### Updated Webpack 
-
-* added `context` so webpack looks in `src/` for files
-* changed `--content-base` in `npm start` script to point to `src` directory
-  * this was need for `src/index.html` to get picked up by webpack-dev-server
-* changed publicPath to just `/`
-* added HtmlWebpackPlugin which outputs html into `dist/`
-
-
-I think we're ready to finally get into react/redux.
+Next - Putting locations on a map.
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 
 const App = React.createClass({
+  getInitialState: function () {
+    return {
+      results: []
+    };
+  },
+
   handleSubmit (e) {
     e.preventDefault();
-    console.log('entered search for: ', this.refs.searchInput.getDOMNode().value);
+
+    let query = this.refs.searchInput.getDOMNode().value;
+    this.executeSearch(query);
+  },
+
+  executeSearch (query) {
+    var req = new XMLHttpRequest();
+    req.addEventListener('load', (e)=>{
+      this.handleSearchResponse(e.target.response);
+    });
+    req.open('GET', `https://api.foursquare.com/v2/venues/explore?client_id=3TQPQPXQALXW3BZNBFFURAWCT4T5UWP4DW5PWJEAE3YBMZZE&client_secret=VTXV5ASAD35D3AJC2YRKRVOOM2BOJC1TGQFZOVNN4IZ3H0CV&v=20150922&ll=40.74,-73.99&radius=3000&intent=browse&venuePhotos=1&section=food&limit=50&query=${query}`);
+    req.send();
+  },
+
+  handleSearchResponse (res) {
+    let response = JSON.parse(res);
+
+    console.log(response);
+
+    if ( response && response.response && response.response.groups && response.response.groups[0] && response.response.groups[0].items ) {
+      this.setState({
+        results: response.response.groups[0].items
+      });
+    }
   },
 
   render () {
+    let { results } = this.state;
+    console.log(results);
+
     return (
       <main>
         <header className='page-header'>
@@ -19,7 +51,13 @@ const App = React.createClass({
           </form>
         </section>
         <section className='search-results'>
-          search results
+          {()=>{
+            return results.map((result)=>{
+              return (
+                <p key={result.venue.id}>{result.venue.name}</p>
+              );
+            });
+          }()}
         </section>
       </main>
     );
